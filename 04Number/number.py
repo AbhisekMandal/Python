ef7568884884966db2797f5ae952fb1c1f6208e4
Abhisek <mandalabhisek20@gmail.com>
2025-06-02 11:23:21 +0530

    Number

---
+++ 04Number/number.py
@@ -0,0 +1,9 @@
+#Number or Numeric values are three types ==> int, float, complex
+x = int(25)
+y = float(26.23)
+z = complex(26j)
+
+#Python has a built-in module called random that can be used to make random numbers.
+#For that we need to import the random module
+import random
+print(random.randrange(1,10)) #display a random number from 1 to 9:
