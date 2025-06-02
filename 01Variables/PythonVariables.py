e8768564216ebc06008e4a62ad5a6471008e08a8
Abhisek <mandalabhisek20@gmail.com>
2025-06-02 10:30:20 +0530

    Variables

---
+++ 01Variables/PythonVariables.py
@@ -0,0 +1,33 @@
+# Variable is a  name of mamory container in which contains some space
+# In Python there is no  variable type.
+#Indentation is importan in python
+name = 'abhisek'
+age = 25
+location = 'Odisha'
+print('Name:',name)
+print('Age:',age)
+print('Location:',location)
+
+# We can assign multiple variable at a time using ','
+name,age,location = 'Akash Mandal',23,'Kolkata'
+print('Name:',name)
+print('Age:',age)
+print('Location:',location)
+
+fruits =['Mango','jackfruit','Orange']
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+print(fruits)
+
+#How to know the types of variables ==> type()
+user = 'Basant'
+age = 10
+weight = 50.65
+married = True
+print(type(user)) # <class 'str'>
+print(type(age)) # <class 'int'>
+print(type(weight))# <class 'float'>
+print(type(married))# <class 'bool'>
+
