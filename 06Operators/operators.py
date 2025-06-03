100d6136db188498407da2d72727a90da97b0b1f
Abhisek <mandalabhisek20@gmail.com>
2025-06-03 10:13:16 +0530

    Operators

---
+++ 06Operators/operators.py
@@ -0,0 +1,67 @@
+#Arithmetic Operators (+,-,*,/,%,//,**)
+print ("addition=",12 + 25)
+print("subtraction=",60 - 25)
+print("multiplecation=",52*5)
+print("division=",25/2)
+print("modulus=",25%2)
+print("exponentiatation=", 4**2)
+print("FloorDivision=",25//3)
+
+#Assignment Operators (=,+=,-=,*=,/=,%=)
+x = 25
+y = 2
+y += x
+print(y)
+
+y -= x
+print(y)
+
+y *= x
+print(y)
+
+y /= x
+print(y)
+
+y %= x
+print(y)
+
+#Comparision Operators : it will give  you bool output
+print(20 == 20)
+print(2 > 5)
+print(2 < 5)
+print(5 != 5)
+print(25 <= 26)
+print(25 >= 26)
+
+#Logical opertos :(and,or,not)
+print(5 > 2 and 6 < 7 )
+print(5 > 2 and 6 > 7 )
+print (not(5 > 2 and 6 < 7 ))
+
+#Identify Operators (is, is not)
+x = ["apple","orange","mango"]
+y = ["apple","orange","mango"]
+z = x
+print(z)
+print(x is z) #true
+print(z is x) #true
+print(x is y) #false
+print(x is not y)
+print(x == y)
+
+#Membership Operators :(in, not in)
+x =["banana","coconut"]
+print("banana" in x) #true
+print("coconut" not in x) # false
+
+
+
+
+
+
+
+
+
+
+
+
